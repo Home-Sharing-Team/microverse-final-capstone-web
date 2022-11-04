3b6e9aab71778353f42ebd4e1b9e180c5e37c51d
Gabriel Santo <gvgesanto2@gmail.com>
2022-11-04 20:48:51 -0300

    feat: Add the initial base styles and color palette

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { initFakeServer } from './services/fake-server.service';
 
+import './styles/global.scss';
+
 initFakeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
