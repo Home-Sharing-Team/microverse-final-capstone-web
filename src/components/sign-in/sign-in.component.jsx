57bde60505210ae56936f5ece445c4b78d7de686
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-11-08 18:25:39 -0600

    Start working login/signin page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+const SignInComponent = () => (
+  <section>
+    <h1>Welcome to the app!</h1>
+  </section>
+);
+
+export default SignInComponent;
